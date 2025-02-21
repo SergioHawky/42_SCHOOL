89d6d8b1212636080aeff5a0f04be00b19c44fc5
SergioHawky <sergioilkiv14@gmail.com>
2025-02-21 19:40:44 +0000

    gravidadde +

--- SO_LONG/gravity.c
+++ SO_LONG/gravity.c
@@ -2,25 +2,25 @@
 
 void apply_gravity(game_data *game)
 {
-    static float gravity_force = 0.01; 
-    static float max_fall_speed = 0.1;
+    //static float gravity_force = 0.01; 
+    //static float max_fall_speed = 0.1;
 
-    if (game->player.gravity_velocity == 0) // iniciar a variavel
-        game->player.gravity_velocity = 0.1;
+    //if (game->player.gravity_velocity == 0) // iniciar a variavel
+    game->player.gravity_velocity = 0.1;
 
     float new_x = game->player.position_x;
-    float new_y = game->player.position_y + game->player.gravity_velocity;
+    float new_y = game->player.position_y + game->player.gravity_velocity - 0.015;
 
     // Aumenta gradualmente a velocidade da queda, mas limita a um valor máximo
-    if (game->player.gravity_velocity < max_fall_speed)
-        game->player.gravity_velocity += gravity_force;
+    //if (game->player.gravity_velocity < max_fall_speed)
+    //    game->player.gravity_velocity += gravity_force;
 
     if (!player_touching_tile(game, new_x, new_y))
     {
         game->player.position_y = new_y;
     }
-    else
+    /*else
     {
         game->player.gravity_velocity = 0.0;
-    }
+    }*/
 }
